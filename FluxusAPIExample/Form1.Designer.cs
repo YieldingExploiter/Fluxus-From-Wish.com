60e880d2551c30788be84820e09764da372ec733
YieldingExploiter <yieldingexploiter@users.noreply.github.com>
2022-06-05 17:16:10 +0200

    first commit

---
+++ FluxusAPIExample/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace FluxusAPIExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.ScriptBox = new System.Windows.Forms.RichTextBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.button3 = new System.Windows.Forms.Button();
+      this.checkBox1 = new System.Windows.Forms.CheckBox();
+      this.checkBox2 = new System.Windows.Forms.CheckBox();
+      this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // ScriptBox
+      // 
+      this.ScriptBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+      this.ScriptBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+      this.ScriptBox.ForeColor = System.Drawing.Color.White;
+      this.ScriptBox.Location = new System.Drawing.Point(12, 12);
+      this.ScriptBox.Name = "ScriptBox";
+      this.ScriptBox.Size = new System.Drawing.Size(519, 214);
+      this.ScriptBox.TabIndex = 0;
+      this.ScriptBox.Text = "// Put your script here";
+      // 
+      // button1
+      // 
+      this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+      this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.button1.ForeColor = System.Drawing.Color.White;
+      this.button1.Location = new System.Drawing.Point(12, 334);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "Execute";
+      this.button1.UseVisualStyleBackColor = false;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // button2
+      // 
+      this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+      this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.button2.ForeColor = System.Drawing.Color.White;
+      this.button2.Location = new System.Drawing.Point(93, 335);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(75, 23);
+      this.button2.TabIndex = 2;
+      this.button2.Text = "Inject";
+      this.button2.UseVisualStyleBackColor = false;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // button3
+      // 
+      this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+      this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.button3.ForeColor = System.Drawing.Color.White;
+      this.button3.Location = new System.Drawing.Point(174, 335);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(75, 23);
+      this.button3.TabIndex = 3;
+      this.button3.Text = "Clear";
+      this.button3.UseVisualStyleBackColor = false;
+      this.button3.Click += new System.EventHandler(this.button3_Click);
+      // 
+      // checkBox1
+      // 
+      this.checkBox1.Location = new System.Drawing.Point(456, 334);
+      this.checkBox1.Name = "checkBox1";
+      this.checkBox1.Size = new System.Drawing.Size(75, 24);
+      this.checkBox1.TabIndex = 4;
+      this.checkBox1.Text = "Internal UI";
+      this.checkBox1.UseVisualStyleBackColor = true;
+      this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+      // 
+      // checkBox2
+      // 
+      this.checkBox2.Location = new System.Drawing.Point(367, 334);
+      this.checkBox2.Name = "checkBox2";
+      this.checkBox2.Size = new System.Drawing.Size(83, 24);
+      this.checkBox2.TabIndex = 5;
+      this.checkBox2.Text = "Unlock FPS";
+      this.checkBox2.UseVisualStyleBackColor = true;
+      this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+      // 
+      // richTextBox2
+      // 
+      this.richTextBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+      this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+      this.richTextBox2.ForeColor = System.Drawing.Color.White;
+      this.richTextBox2.Location = new System.Drawing.Point(12, 232);
+      this.richTextBox2.Name = "richTextBox2";
+      this.richTextBox2.ReadOnly = true;
+      this.richTextBox2.Size = new System.Drawing.Size(519, 96);
+      this.richTextBox2.TabIndex = 6;
+      this.richTextBox2.Text = "";
+      this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+      // 
+      // label1
+      // 
+      this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+      this.label1.Location = new System.Drawing.Point(0, 361);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(542, 21);
+      this.label1.TabIndex = 7;
+      this.label1.Text = "Unofficial Dark Theme Remake of Fluxus Lite by Yielding#3961";
+      this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+      this.ClientSize = new System.Drawing.Size(542, 382);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.richTextBox2);
+      this.Controls.Add(this.checkBox2);
+      this.Controls.Add(this.checkBox1);
+      this.Controls.Add(this.button3);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.ScriptBox);
+      this.ForeColor = System.Drawing.Color.White;
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.Name = "Form1";
+      this.Text = "Fluxus from Wish.com";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox ScriptBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    private System.Windows.Forms.Label label1;
+  }
+}
+
---
+++ FluxusAPIExample/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FluxusAPIExample
+{
+  public partial class Form1 : Form
+  {
+    FluxAPI.API API = new FluxAPI.API();
+
+    public Form1()
+    {
+      InitializeComponent();
+      Random random = new Random();
+      switch (random.Next(0,5))
+      {
+        case 1:
+          Text = "Fluxus on a Diet";
+          break;
+        case 2:
+          Text = "Fluxus Lite: Now in dark";
+          break;
+        case 3:
+          Text = "Fluxus Lite Dark: Not even fastcoloredtextbox";
+          break;
+        case 4:
+          Text = "Fluxus Lite with less eye-pain";
+          break;
+        default:
+          Text = "Fluxus from Wish.com";
+          break;
+      }
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      API.Execute(ScriptBox.Text);
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      API.Inject();
+    }
+    private void button3_Click(object sender, EventArgs e)
+    {
+      ScriptBox.Text = "";
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      FluxAPI.API.OnInputRecived += delegate (string input) //OnInputRecived will be called when the DLL sends data to the UI
+      {
+        this.Invoke(new MethodInvoker(delegate //THIS IS REQUIRED, YOU WILL ERROR WITHOUT THIS
+              {
+            richTextBox2.Text = richTextBox2.Text + $"[Console {DateTime.Now.ToString("HH:mm:ss")}] {input}\n";
+          }));
+      };
+
+      FluxAPI.API.OnInject += delegate ()
+      {
+        // API.Execute("print('Fluxus has injected!')"); //Execute this when Fluxus has injected.
+
+        if (checkBox1.Checked) //If the Internal UI toggle is checked, toggle it!
+          API.ToggleInternalUI();
+
+        if (checkBox2.Checked) //If Unlock FPS is checked, enable it!
+          API.SetFPSCap(); //If you do not give a number, it will set it to max.
+      };
+    }
+
+    private void checkBox1_CheckedChanged(object sender, EventArgs e)
+    {
+      API.ToggleInternalUI(); //Toggles the internal UI from on/off
+    }
+
+    private void checkBox2_CheckedChanged(object sender, EventArgs e)
+    {
+      if (checkBox2.Checked) //If this is true, the user wants to unlock the fps
+        API.SetFPSCap(); //If you do not give a number, it will set it to max.
+      else //Its false so they want to lock it again
+        API.SetFPSCap(60); //Roblox cap is 60 fps, so restore it to 60.
+    }
+
+    private void richTextBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
